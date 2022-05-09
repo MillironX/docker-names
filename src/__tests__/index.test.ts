d94d76c840e60d0fdb72429c8f1089921b346394
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-05-09 08:46:32 -0500

    Apply Prettier formatting to tests
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,21 +1,20 @@
-import {generateName, generateNameWithNumber, left, right} from "../index";
+import { generateName, generateNameWithNumber, left, right } from "../index";
 
-
-describe('Docker names', () => {
-  test('should generate a random name', () => {
+describe("Docker names", () => {
+  test("should generate a random name", () => {
     const name = generateName();
-    const parts = name.split('-');
+    const parts = name.split("-");
     expect(parts).toHaveLength(2);
     expect(left).toContain(parts[0]);
     expect(right).toContain(parts[1]);
-  })
+  });
 
   test("should generate a random name with a number", () => {
     const nameWithNumber = generateNameWithNumber();
-    const parts = nameWithNumber.split('-');
+    const parts = nameWithNumber.split("-");
     expect(parts).toHaveLength(3);
     expect(left).toContain(parts[0]);
     expect(right).toContain(parts[1]);
     expect(parseInt(parts[2])).toBeGreaterThan(0);
-  })
-})
+  });
+});
